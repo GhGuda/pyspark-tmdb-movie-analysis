ca4cf3b98da584665d8d2650d9f7da103648fdc8
GhGuda <toyboipressure1@gmail.com>
2026-02-26 00:25:00 +0000

    test: fix imports and add pytest path setup

--- app/extraction/fetch_api_data.py
+++ app/extraction/fetch_api_data.py
@@ -6,8 +6,8 @@ from typing import Iterable
 
 import requests
 
-from configs import settings
-from app.extraction.helper import save_json
+from app.configs import settings
+from app.extraction.helpers import save_json
 from app.utils.logging import get_logger
 
 
